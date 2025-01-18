766f8efafdb5035fc617d828551fbb9204f6bb8d
Rashi Agrawal <rashi.agrawal_cs21@gla.ac.in>
2025-01-18 15:02:50 +0530

    program1

---
+++ PracticeProblem.java
@@ -0,0 +1,14 @@
+public class PracticeProblem{    
+public static void main(String args[])   
+{  
+int number=10;
+System.out.print("List of odd numbers from 1 to "+number+": ");  
+for (int i=1; i<=number; i++)   
+{    
+if (i%2!=0)   
+{  
+System.out.print(i + " ");  
+}  
+}  
+}  
+}  
